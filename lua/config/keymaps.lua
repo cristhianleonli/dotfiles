6a5f9ed21463ccdf14e81c1ea793e97d944c1c95
Cristhian Leon <cris.leonardoleon@gmail.com>
2026-05-16 08:54:58 -0300

    Initial config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+vim.opt.backspace = '2'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+-- vim.opt.autowrite = true
+-- vim.opt.autoread = true
+-- vim.opt.showcmd = true
+-- vim.opt.laststatus = 2
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+-- vim.opt.shiftround = true
+-- vim.opt.shiftwidth = 2
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
