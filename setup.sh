a7e7a993e58608f03253e2c94a0c9b7ea33edcca
Cristhian Leon <cris.leonardoleon@gmail.com>
2026-06-02 21:59:37 -0300

    Adds starship

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ DOTFILES="$HOME/.config/dotfiles"
 
 ln -sfn "$DOTFILES/nvim" ~/.config/nvim
 ln -sfn "$DOTFILES/kitty" ~/.config/kitty
+ln -sfn "$DOTFILES/starship.toml" ~/.config/starship.toml
 ln -sfn "$DOTFILES/aliases.zsh" ~/.aliases
 
