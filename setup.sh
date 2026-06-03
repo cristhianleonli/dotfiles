1253b2422d011fdb9ca8efcb206d404c65611780
Cristhian Leon <cris.leonardoleon@gmail.com>
2026-06-02 21:46:34 -0300

    Adds kitty and zsh aliases command

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 # Move config files
 
 DOTFILES="$HOME/.config/dotfiles"
-ln -sfn "$DOTFILES/nvim" ~/.config/nvim 
+
+ln -sfn "$DOTFILES/nvim" ~/.config/nvim
+ln -sfn "$DOTFILES/kitty" ~/.config/kitty
+ln -sfn "$DOTFILES/aliases.zsh" ~/.aliases
+
